d2807a0494fd21655df78378442ad7755f365339
Tracy Falba <tfalba@mac.com>
2020-11-28 13:34:15 -0500

    tried new ways of removing listeners but didn't work -- uploaded more flowers -- added a timer but can't stop it -- only restarts with refresh on restart button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let animals = new Array()
-const flowers = new Array()
+let flowers = new Array()
+const timer = document.querySelector('#timer')
+
 
 function shuffleArray (array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -8,111 +10,125 @@ function shuffleArray (array) {
   }
 }
 function wait (ms) {
-  let d = new Date()
-  var d2 = null
+  const d = new Date()
+  let d2 = null
   do { d2 = new Date() }
   while (d2 - d < ms)
 }
 
+function toggleMe (e) {
+  const toggleChoice = e.innerText
+  console.log(e.classList)
+  e.classList.toggle(toggleChoice)
+  console.log(e.classList)
+  console.log(toggleChoice)
+}
+/* --------------------------------------------- save this if need later -------------------------------------------- */
+function removeMe (e) {
+  const toggleChoice = e.innerText
+  e.classList.remove(toggleChoice)
+}
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+/*             Function for overall gameplay -- calls in some of above functions (toggleMe, shuffleArray)             */
+/* ------------------------------------------------------------------------------------------------------------------ */
+
 function playMatch (roundSize, picChoices, picChoice) {
-  // make roundSize an input choice for user as long as it is even and within a certain amount
-  // where images are available
-  const display = new Array()
-  const shuffle = new Array()
-  const start = new Array()
+
+
+  const display = []
+  const shuffle = []
+  const start = []
+  const open = []
+  const matched = []
+  // var start = Date.now();
+  //   setInterval(function() {
+  //     var delta = Date.now() - start; 
+  //     output(new Date().toUTCString());
+  // }, 1000);
+
 
   for (let i = 0; i < roundSize / 2; i++) {
     picChoices[i] = picChoice + i
     picChoices[i + (roundSize / 2)] = picChoice + i
   }
-  for (let i = 0; i < roundSize; i++) {
-    shuffle[i] = i
-  }
+
+  /* ---------------------- This hides all the squares from the opening display once game starts ---------------------- */
 
   for (let i = 0; i < 16; i++) {
     start[i] = document.querySelector('#start' + i)
     start[i].classList.add('hideme')
   }
+
+  for (let i = 0; i < roundSize; i++) {
+    shuffle[i] = i
+  }
+  console.log(open)
   shuffleArray(shuffle)
 
+  /* --------- Display array is used to show number of cards played and assign them all their inner text value -------- */
+  /* ------------------------------------------ this gets done after shuffle ------------------------------------------ */
   for (let i = 0; i < roundSize; i++) {
     display[i] = document.querySelector('#display' + i)
     display[i].classList.remove('hideme')
     display[i].innerText = picChoices[shuffle[i]]
   }
+  for (let i = roundSize; i < 36; i++) {
+    display[i] = document.querySelector('#display' + i)
+    display[i].classList.add('hideme')
+  }
+
+  /* ------------------------------------------------------------------------------------------------------------------ */
+  /*                                        Here's where listening process beings                                       */
+  /* ------------------------------------------------------------------------------------------------------------------ */
+
   const container = document.querySelector('#game-display')
 
   let countClicks = 0
-  const toggleChoice = ''
+  let match = 0
   let contentArray = []
-  const targetArray = []
+  let targetArray = []
+  const displayResults = document.querySelector('#results')
 
   /* --------------------------- May need to consider making event listeners for each card. --------------------------- */
 
-  // container.addEventListener('click', function handler (event) {
-  //   event.preventDefault()
-  //   // if (countClicks <= 2) {
-  //   toggleChoice = event.target.innerText
-  //   console.log(event.target.classList)
+  /* ------------------------------------------------------------------------------------------------------------------ */
+  /*             Different approach for listening by setting tracking on open and match arrays                                         */
+  /* ------------------------------------------------------------------------------------------------------------------ */
 
-  //   event.target.classList.toggle(toggleChoice)
+  /* ------------------ In above code before listener happens, initialize open array and match array ------------------ */
+  //   const openTotal = open => open.reduce((a, b) => a + b, 0)
+  //   matched[1] = 1
+  //   matched[2] = 1
+  //   const seven = 7
+  //   for (let i = 0; i < roundSize; i++) {
+  //     if (matched[i] === 0 && openTotal(open) < 3) {
 
-  //   console.log(toggleChoice)
-  //   console.log(countClicks)
-  //   console.log(event.target.classList)
+  //       display[i].addEventListener('click', function handler (event) {
+  //         toggleMe(event.target)
+  //         countClicks++
+  //         open[i] = 1
+  //         targetArray.push(event.target)
+  //         contentArray.push(event.target.innerText)
+  //         console.log('These are the targets that have been clicked so far: ', targetArray)
+  //         console.log('This is the content that has been clicked so far: ', contentArray)
+  //         console.log('This is the number of clicks so far: ', countClicks)
+  //         console.log('this is the setting for the current target: ', open[i])
+  //         console.log('this is the accumulation of the settings', open)
+  //         console.log('this is the sum of all open: ', openTotal(open))
+  //         event.target.removeEventListener('click', handler)
+  //       }
+  //       )}
+  //      else {
+  //       display[i].addEventListener('click', function handler (event) {
+  //         event.target.removeEventListener('click', handler)
+  //         console.log('I am here. This is the event target: ', event.target)
+  //       })
+  //     }
+  //   }
+  // }
 
-  //   countClicks += 1
-  //   console.log(countClicks)
-  //   targetArray.shift()
-  //   toggleArray.shift()
-  //   targetArray.push(event.target)
-  //   toggleArray.push(toggleChoice)
-  //   // if ((toggleArray[0] !== toggleArray[1]) && countClicks > 2) {
-  //   //   console.log("NO MATCH")
-  //   //   // wait(2000)
-  //   //   function flipOver () {
-  //   //     targetArray[0].classList.toggle(toggleArray[0])
-  //   //     targetArray[1].classList.toggle(toggleArray[1])
-  //   //   }
-  //   //   setTimeout(flipOver(), 2000)
-  //   // }
-  //   const resultsDiv = document.querySelector('#results')
-  //   resultsDiv.innerText = `'You have clicked ${countClicks} times!'`
-  //   console.log('this is array of innertext: ', toggleArray)
-  //   console.log('this is the array of past two event targets: ', targetArray)
-
-  //   // function matchCheck () {
-  //   //   event.target.classList.toggle(toggleChoice)
-  //   //   console.log('I am here!!', countClicks)
-  //   // }
-
-  //   // if (countClicks === 3) {
-  //   //   wait(2000)
-  //   //   matchCheck()
-  //   //   countClicks = 0
-  //   // }
-
-  //   // if (countClicks === 3) {
-  //   //   event.currentTarget.removeEventListener(event.type, handler)
-  //   //   countClicks = 0
-  //   //   container.addEventListener('click', function handler(event) {
-  //   //     event.target.classList.toggle(toggleChoice)
-  //   //   })
-  //   // }
-  // })
-
-  function toggleMe (e) {
-    const toggleChoice = e.innerText
-    console.log(e.classList)
-    e.classList.toggle(toggleChoice)
-    console.log(e.classList)
-    console.log(toggleChoice)
-  }
-
-  function removeMe (e) {
-    const toggleChoice = e.innerText
-    e.classList.remove(toggleChoice)
-  }
+  /* ------------------------------ This section is the event listener over all the cards ----------------------------- */
 
   for (const card of display) {
     card.addEventListener('click', function handler (event) {
@@ -125,30 +141,18 @@ function playMatch (roundSize, picChoices, picChoice) {
       console.log('This is the number of clicks so far: ', countClicks)
       // event.target.removeEventListener('click', handler)
       if (countClicks % 2 === 0) {
-        // matchCheck(contentArray, targetArray)
+      // matchCheck(contentArray, targetArray) -- write a function
         if (contentArray[0] === contentArray[1]) {
           console.log('We have a match!!')
-          // targetArray[countClicks-1].classList.add('shimmer')
-          // targetArray[countClicks-2].classList.add('shimmer')
-          // targetArray = []
-          // contentArray = []
+          match++
+          console.log(match)
           contentArray.shift()
           contentArray.shift()
-          console.log(targetArray[countClicks-1])
-          console.log(targetArray[countClicks-2])
-          targetArray[countClicks-2].removeEventListener('click', handler)
-          targetArray[countClicks-1].removeEventListener('click', handler)
-          // targetArray[countClicks-2].removeEventListener('click', handler)
-          // targetArray.shift()
-          // targetArray.shift()
+          targetArray[countClicks - 2].removeEventListener('click', handler)
+          targetArray[countClicks - 1].removeEventListener('click', handler)
         } else {
           console.log("We don't match!!")
-
           contentArray = []
-          // need to toggle first two after third click
-          // targetArray[0].addEventListener('click', handler(e))
-          // targetArray[1].addEventListener('click', handler(e))
-          // targetArray = []
         }
       } else if (countClicks > 2) {
         if (targetArray[countClicks - 2].innerText !== targetArray[countClicks - 3].innerText) {
@@ -158,36 +162,16 @@ function playMatch (roundSize, picChoices, picChoice) {
           toggleMe(targetArray[countClicks - 3])
           // targetArray[countClicks - 3].addEventListener('click', handler(event))
           // targetArray[countClicks - 2].addEventListener('click', handler(event))
-          // countClicks -= 2
-          // targetArray.shift()
-          // targetArray.shift()
-
           console.log(targetArray)
-        }
-        else if (targetArray[countClicks -2].innerText === targetArray[countClicks -3].innerText) {
-          targetArray[countClicks-3].classList.add('shimmer')
-          targetArray[countClicks-2].classList.add('shimmer')
+        } else if (targetArray[countClicks - 2].innerText === targetArray[countClicks - 3].innerText) {
+          targetArray[countClicks - 3].classList.add('shimmer')
+          targetArray[countClicks - 2].classList.add('shimmer')
         }
       }
+      displayResults.innerText = `Number of Clicks: ${countClicks}`
     })
   }
 }
-
-// {
-//   if (contentArray[0] === contentArray[1]) {
-//     console.log('We have a match!!')
-//     targetArray = []
-//     contentArray = []
-//   } else {
-//     console.log("We don't match!!")
-//     targetArray = []
-//     contentArray = []
-//   }
-// }
-
-// e.target.removeEventListener('click', handler)
-// if first click then keep listeners going
-// if second click then check for match
 
 /* --------------------------------------- turning handler on and off example --------------------------------------- */
 
@@ -197,31 +181,38 @@ function playMatch (roundSize, picChoices, picChoice) {
 // })
 
 const consoleDisplay = document.querySelector('#console-display')
+function gameStart (round, picClass, nameOfClass) {
+  intro.classList.add('hideme')
+  animals = []
+  flowers = []
+  var seconds = 0
+  setInterval(function () {
+    timer.innerHTML = "Seconds elapsed: " + seconds++
+  }, 1000)
+  playMatch(round, picClass, nameOfClass)
+}
+
 consoleDisplay.addEventListener('click', function (event) {
   if (event.target.id === 'animals-easy') {
-    intro.classList.add('hideme')
-    animals = []
-    playMatch(16, animals, 'animal')
+    gameStart(16, animals, 'animal')
   }
   if (event.target.id === 'animals-medium') {
-    intro.classList.add('hideme')
-    animals = []
-    console.log('Playing medium')
-    playMatch(24, animals, 'animal')
+    gameStart(24, animals, 'animal')
   }
   if (event.target.id === 'animals-difficult') {
-    intro.classList.add('hideme')
-    playMatch(36, animals, 'animal')
-    animals = []
+    gameStart(36, animals, 'animal')
   }
-
   if (event.target.id === 'flowers-easy') {
-    intro.classList.add('hideme')
-    playMatch(16, flowers, 'flower')
-    animals = []
+    gameStart(16, flowers, 'flower')
   }
-
+  if (event.target.id === 'flowers-medium') {
+    gameStart(24, flowers, 'flower')
+  }
+  if (event.target.id === 'flowers-difficult') {
+    gameStart(36, flowers, 'flower')
+  }
   if (event.target.id === 'reset') {
     document.location.reload()
   }
+  // make this last a function that clears all toggles to the initial position.
 })
